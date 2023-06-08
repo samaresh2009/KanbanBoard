cc0aff3765bf5e4d2c529ff00a082e414514c937
samaresh2009 <samareshsahoo@gmail.com>
2023-06-08 20:25:31 +0530

    Dynamic ticket id

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let addBtn = document.querySelector(".add-btn");
 let modal = document.querySelector(".modal-cont");
 let allPriCont = document.querySelectorAll(".priority-cont");
+let textArea = document.querySelector(".textarea-cont");
+let mainCont = document.querySelector('.main-cont');
+let modalPriorityColor = 'black';
 let addModal = true;
+let uid = new ShortUniqueId();
 
 addBtn.addEventListener("click", function () {
     if (addModal) {
@@ -20,5 +24,26 @@ for (let i = 0; i < allPriCont.length; i++) {
             }
         }
         allPriCont[i].classList.add('active');
+        modalPriorityColor = allPriCont[i].classList[1];
     })
+}
+
+textArea.addEventListener('keydown', function (e) {
+    if (e.key == "Enter") {
+        let task = textArea.value;
+        createTicket(task);
+        textArea.value = "";
+        modal.style.display = "none";
+        addModal = !addModal;
+
+    }
+});
+
+function createTicket(task) {
+    let ticketCont = document.createElement('div');
+    ticketCont.setAttribute("class", "ticket-cont");
+    ticketCont.innerHTML = `<div class="ticket-color ${modalPriorityColor}"></div>
+                            <div class="ticket-id">#${uid()}</div>
+                            <div class="task-area">${task}</div>`;
+    mainCont.appendChild(ticketCont);
 }
